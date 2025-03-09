03511d954599b6d4a90f2e5f74b09de21512935d
Elijah <elijah.macharia@student.moringaschool.com>
2025-03-09 21:42:15 +0300

    Functions

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(block) {
+    return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    
+    if (typeof distance !== 'number' || distance < 0) {
+        return 'Invalid distance';
+    }
+    if (distance <= 400) {
+        return 0;
+    } 
+    else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } 
+    else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } 
+    else {
+        return 'cannot travel that far';
+    }
+}
+
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+        console.log("I am a named function!");
+    };
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("I am an anonymous function!");
+    };
+}
